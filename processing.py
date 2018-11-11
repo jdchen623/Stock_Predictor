b2f533a010a6e2856c8fa9644b1b54b37a67bd4f
Kathleen Kenealy <kathleen@gamecommerce.com>
2018-11-11 14:35:38 -0800

    Empty processing script.

---
+++ processing.py
@@ -0,0 +1,53 @@
+import collections
+
+import numpy as np
+
+import util
+import svm
+
+def main():
+    train_messages, train_labels = util.load_spam_dataset('../data/ds6_train.tsv')
+    val_messages, val_labels = util.load_spam_dataset('../data/ds6_val.tsv')
+    test_messages, test_labels = util.load_spam_dataset('../data/ds6_test.tsv')
+
+    dictionary = create_dictionary(train_messages)
+
+    util.write_json('./output/p06_dictionary', dictionary)
+
+    train_matrix = transform_text(train_messages, dictionary)
+
+    np.savetxt('./output/p06_sample_train_matrix', train_matrix[:100,:])
+
+    val_matrix = transform_text(val_messages, dictionary)
+    test_matrix = transform_text(test_messages, dictionary)
+
+    naive_bayes_model = fit_naive_bayes_model(train_matrix, train_labels)
+
+    naive_bayes_predictions = predict_from_naive_bayes_model(naive_bayes_model, test_matrix)
+
+    np.savetxt('./output/p06_naive_bayes_predictions', naive_bayes_predictions)
+
+    naive_bayes_accuracy = np.mean(naive_bayes_predictions == test_labels)
+
+    print('Naive Bayes had an accuracy of {} on the testing set'.format(naive_bayes_accuracy))
+
+    top_5_words = get_top_five_naive_bayes_words(naive_bayes_model, dictionary)
+
+    print('The top 5 indicative words for Naive Bayes are: ', top_5_words)
+
+    util.write_json('./output/p06_top_indicative_words', top_5_words)
+
+    optimal_radius = compute_best_svm_radius(train_matrix, train_labels, val_matrix, val_labels, [0.01, 0.1, 1, 10])
+
+    util.write_json('./output/p06_optimal_radius', optimal_radius)
+
+    print('The optimal SVM radius was {}'.format(optimal_radius))
+
+    svm_predictions = svm.train_and_predict_svm(train_matrix, train_labels, test_matrix, optimal_radius)
+
+    svm_accuracy = np.mean(svm_predictions == test_labels)
+
+    print('The SVM model had an accuracy of {} on the testing set'.format(svm_accuracy, optimal_radius))
+
+if __name__ == "__main__":
+    main()
